c1968415ff94636b1ae9ab8d72be094f7a062d08
yingming006 <yingming006@foxmail.com>
2020-11-10 10:46:31 +0800

    update deploy script

--- deploy-hexo-and-git.sh
+++ deploy-hexo-and-git.sh
@@ -3,8 +3,8 @@ echo "#########deploy hexo#########"
 hexo clean && hexo g && hexo d
 
 echo "#########deploy git#########"
-host=$(git status | awk -F\' '{print $2}' | awk -F/ '{print $1}' | sed -n 2p)
-branch=$(git status | awk -F\' '{print $2}' | awk -F/ '{print $2}' | sed -n 2p)
+host=$(git status | sed -n 2p | awk -F\' '{print $2}' | awk -F/ '{print $1}')
+branch=$(git status | sed -n 2p | awk -F\' '{print $2}' | awk -F/ '{print $2}')
 git add .
 read -p "commit massage:" message
 git commit -m "$message"
