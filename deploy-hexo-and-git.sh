5ccc45007a47b6b8384fa7fb38c5343da4ed09e1
yingming006 <yingming006@foxmail.com>
2021-01-06 13:16:18 +0800

    update

--- deploy-hexo-and-git.sh
+++ deploy-hexo-and-git.sh
@@ -6,6 +6,6 @@ echo "#########deploy git#########"
 host=$(git status | sed -n 2p | awk -F\' '{print $2}' | awk -F/ '{print $1}')
 branch=$(git status | sed -n 2p | awk -F\' '{print $2}' | awk -F/ '{print $2}')
 git add .
-read -p "commit massage:" "$1"
-git commit -m "$1"
+read -p "commit massage:" $1
+git commit -m $1
 git push "$host" "$branch"
